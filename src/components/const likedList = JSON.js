140975bb7d4dfb5993fc85af69e2c447cc94004d
lenilaurae <lenilaurae.gmail.com>
2023-12-07 11:31:57 +0100

    localstorage comment

--- src/components/const likedList = JSON.js
+++ src/components/const likedList = JSON.js
@@ -1,5 +1,7 @@
+//apunte LocalStorage y React
+
 const likedList = JSON.parse(localStorage.getItem("liked")) ?? [];
 
 const nextLikedList = likedList.concat({ id: 5, liked: true });
-const nextLikedList = likedList.filter((obj) => texto !== "");
+//const nextLikedList = likedList.filter((obj) => texto !== "");
 localStorage.setItem("liked", JSON.stringify(nextLikedList));
